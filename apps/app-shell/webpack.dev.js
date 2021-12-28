34b7ecdb74bcd7632c231939e1d8c1895aa9a36a
AdrianApan <mail@adrianapan.com>
2021-12-28 17:03:29 +0000

    ♻️ update webpack devtool

--- apps/app-shell/webpack.dev.js
+++ apps/app-shell/webpack.dev.js
@@ -5,7 +5,7 @@ const common = require('./webpack.common.js')
 
 module.exports = merge(common, {
   mode: 'development',
-  devtool: 'inline-source-map',
+  devtool: 'cheap-module-source-map',
   cache: false,
   optimization: {
     minimize: false,
--- apps/two/webpack.dev.js
+++ apps/two/webpack.dev.js
@@ -6,7 +6,7 @@ const common = require('./webpack.common.js')
 
 module.exports = merge(common, {
   mode: 'development',
-  devtool: 'inline-source-map',
+  devtool: 'cheap-module-source-map',
   cache: false,
   optimization: {
     minimize: false,
