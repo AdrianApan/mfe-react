f2fcc8dedf26acbfb1e35af86f5901969353c3a6
AdrianApan <mail@adrianapan.com>
2021-12-30 13:25:53 +0000

    ✏️ update vertical mfe content

--- apps/vertical/src/App.tsx
+++ apps/vertical/src/App.tsx
@@ -14,16 +14,8 @@ const App = () => (
       border: `2px dashed ${VERTICAL_MFE_COLOR}`,
     }}
   >
-    <Typography variant="h5" mb={1}>
+    <Typography variant="h5" p={2} align="center">
       This content lives in the Vertical MFE
-    </Typography>
-
-    <Typography variant="body2">
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-      labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-      laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-      voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
-      non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
     </Typography>
 
     <Label color={VERTICAL_MFE_COLOR} label={VERTICAL_MFE_LABEL} />
